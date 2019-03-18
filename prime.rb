30ba1f5b491011f71194c78df0b3d4b8b671132a
Rob O'Gara <rgo594@gmail.com>
2019-03-18 14:36:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,11 @@
-
 def prime?(num)
-return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
 end
+
+puts prime?(105557)
